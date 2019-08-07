4e8d800e549ff802eea91b5511b284cdb2938581
nataliia-v <nataly.verbenskaya@gmail.com>
2019-08-07 15:29:42 +0300

    added styles and event on buttom important

--- src/components/search-panel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function SearchPanel(props) {
-  return (
-      <input placeholder="search"/>
-  );
-}
-
-export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './components/todo-list';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
 
 
-
-const App = () => {
-
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id:1 },
-    { label: 'Eat Pizza', important: false, id:2 },
-    { label: 'Skiing', important: true, id:3 }
-  ];
+import App from './components/app';
 
 
-  return(
-      <div>
-        <AppHeader/>
-        <SearchPanel/>
-        <TodoList todos={todoData}/>
-      </div>
-  )
-};
-
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
