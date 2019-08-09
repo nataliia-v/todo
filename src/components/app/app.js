879f187584185092ed09c77c1cd2652101a48fa5
nataliia-v <nataly.verbenskaya@gmail.com>
2019-08-09 15:10:41 +0300

    transformed onToggleDone function for 'importan' and 'done '

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-
 import AppHeader from '../app/../app-header';
 import SearchPanel from '../app/../search-panel';
 import TodoList from '../app/../todo-list';
 import ItemStatusFilter from '../app/../item-status-filter';
 import './app.css';
 import AddItem from "../add-item";
-
-
 
 export default class App extends Component {
 
@@ -28,7 +25,6 @@ export default class App extends Component {
       done: false,
       id: this.maxId++,
     };
-
   }
 
   deleteItem = (id) => {
@@ -59,37 +55,39 @@ export default class App extends Component {
 
   };
 
+  toggleProprty = (arr, id, propName ) => {
+
+    const idx = arr.findIndex((el) => el.id === id);
+    const oldItem = arr[idx];
+    const newItem = { ...oldItem, [propName]: !oldItem[propName] };
+
+    return [
+      ...arr.slice(0, idx),
+      newItem,
+      ...arr.slice(idx + 1)
+    ];
+  };
+
   onToggleImportant = (id) => {
-    console.log('important')
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProprty(todoData, id, 'important'),
+      }
+    })
   };
 
   onToggleDone = (id) => {
     this.setState(({ todoData }) => {
-      const idx = todoData.findIndex((el) => el.id === id);
-      const oldItem = todoData[idx];
-      const newItem = { ...oldItem, done: !oldItem.done };
-
-      const newArray = [
-        ...todoData.slice(0, idx),
-          newItem,
-        ...todoData.slice(idx + 1)
-      ];
-
       return {
-        todoData: newArray,
+        todoData: this.toggleProprty(todoData, id, 'done'),
       }
-
     })
   };
 
   render() {
     const {todoData} = this.state;
-
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
-
-
-
     return (
         <div className="todo-app">
           <AppHeader toDo={todoCount} done={doneCount} />
@@ -97,13 +95,9 @@ export default class App extends Component {
             <SearchPanel />
             <ItemStatusFilter />
           </div>
-
           <TodoList todos={this.state.todoData} onToggleImportant={this.onToggleImportant} onToggleDone = {this.onToggleDone} onDeleted = {this.deleteItem} />
           <AddItem onAddItem = {this.addItem}/>
         </div>
     );
   }
 }
-
-
-
