c8d37d100f42b1d602318fa5ba994d61ed32fbef
nataliia-v <nataly.verbenskaya@gmail.com>
2019-08-09 16:08:30 +0300

    controlled components

--- src/components/add-item/index.js
+++ src/components/add-item/index.js
@@ -14,16 +14,23 @@ export default class AddItem extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.onAddItem(this.state.label)
+    this.props.onAddItem(this.state.label);
+    this.setState({
+      label: ''
+    })
   };
 
   render() {
-    const {onAddItem} = this.props;
 
     return (
         <form className='item-add-form d-flex' onSubmit={this.onSubmit}>
-          <input onChange={this.onLabelChange} type="text" className='form-control' placeholder="My todo"/>
-          <button className='btn btn-outline-secondary' >Add</button>
+          <input onChange={this.onLabelChange}
+                 type="text"
+                 className='form-control'
+                 placeholder="My todo"
+                 value={this.state.label} ///для того, чтобы сделать элкмкнт контролируемым
+          />
+          <button className='btn btn-outline-secondary'>Add</button>
         </form>
     );
   }
